694dc894235dc864e929cc5726fbcc9cd40fa452
zakapong <zaka.pong@yahoo.com>
2016-07-04 13:00:38 +0200

    middleware

--- server.js
+++ server.js
@@ -2,20 +2,7 @@ var express = require('express');
 var app= express();
 PORT= 3000;
 
-
-var middleware= {
-   requireAuthentication: function(req, res, next){
-  console.log('private route hit');
-
-next();
-   },
-   logger: function(req, res, next){
-
-   	console.log('Request: '+new Date().toString() +' '+req.method +' '+ req.originalUrl);
-   	next();
-   }
-
-};
+var middleware= require('./middleware.js');
 
 //app.use(middleware.requireAuthentication);
 // app.get('/', function(req, res){
